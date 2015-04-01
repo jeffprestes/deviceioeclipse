194c14d05f0c0c6a692c2ff80ee6e16998a3883a
Jeff Prestes <jeffprestes@gmail.com>
2015-04-01 11:15:25 -0400

    Switch class and Device I/O lib added

---
+++ src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Switch.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.paypal.developer.jeffprestes.demo.deviceioeclipse;
+
+/**
+ * Class that represents a switch to our light bulb
+ * @author jprestes
+ *
+ */
+public class Switch {
+
+}
