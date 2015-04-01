99141a14d4a77b8962ac4f281a51533193ef9d96
Jeff Prestes <jeffprestes@gmail.com>
2015-04-01 10:52:20 -0400

    Initial class

---
+++ src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Start.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.paypal.developer.jeffprestes.demo.deviceioeclipse;
+
+/**
+ * @author jprestes
+ * Initial class of our application
+ */
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
