9f4ba684cf8e463104c278a2c37e7496af0f53d8
Jeff Prestes <jeffprestes@gmail.com>
2015-04-01 11:32:34 -0400

    New usages for our class

--- src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Start.java
+++ src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Start.java
@@ -20,6 +20,12 @@ public class Start {
 		mySwitch.switchOn();
 		
 		System.out.println("What is the status of bulb? It is: " + mySwitch.getStatus());
+		
+		mySwitch.switchOff();
+		
+		mySwitch.blink(3);
+		
+		mySwitch.blink(10, 500);
 	}
 
 }
