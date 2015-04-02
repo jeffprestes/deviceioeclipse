1aa82517974d099943aafc68bb6e2b252513e6ae
Jeff Prestes <jeffprestes@gmail.com>
2015-04-01 20:03:00 -0400

    initial instructions

--- src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Start.java
+++ src/com/paypal/developer/jeffprestes/demo/deviceioeclipse/Start.java
@@ -3,6 +3,8 @@
  */
 package com.paypal.developer.jeffprestes.demo.deviceioeclipse;
 
+import com.paypal.developer.jeffprestes.demo.deviceioeclipse.Switch;
+
 /**
  * @author jprestes
  * Initial class of our application
@@ -15,6 +17,20 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		
+		Switch mySwitch = new Switch();
+		
+		mySwitch.switchOn();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		mySwitch.switchOff();
+		
 	}
 
 }
